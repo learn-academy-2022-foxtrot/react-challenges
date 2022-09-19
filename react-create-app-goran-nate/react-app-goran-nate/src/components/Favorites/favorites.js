0adec673fa959116db6b2283e9e6e0e342431d6f
Goran SD <g.cvetic23@gmail.com>
2022-09-19 12:21:56 -0700

    create react app challenge complete

---
+++ react-create-app-goran-nate/react-app-goran-nate/src/components/Favorites/favorites.js
@@ -0,0 +1,12 @@
+const Favorites = () => {
+  return (
+    <>
+      <div>Top Five Planets:</div>
+      <li>Jupiter</li>
+      <li>Saturn</li>
+      <li>Neptune</li>
+    </>
+  );
+};
+
+export default Favorites;
