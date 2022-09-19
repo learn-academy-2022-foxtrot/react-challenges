5f462894bbb8c156cc81f0456087133fc6456b75
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-09-19 16:07:08 -0700

    completed challenge

---
+++ react-state-gc-nr/react-state-nate-goran/src/Boxes/box2.js
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+const Box2 = () => {
+
+    const [color, setColor] = useState("white")
+    const [counter, setCounter] = useState(0)
+
+    const changeBoxColor = () => {
+        const arrayOfColors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"]
+        setCounter(counter + 1)
+        if (counter == arrayOfColors.length) {
+            setCounter(0)
+        }
+        return setColor(arrayOfColors[counter])
+    }
+    return (
+        <>
+            <div onClick={changeBoxColor} style={{ backgroundColor: color, color: "black", border: "1px solid black", width: "400px", height: "400px", margin: "auto" }}>white</div>
+        </>)
+}
+export default Box2
