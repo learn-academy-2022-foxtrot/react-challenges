4bfa49e6dcf87aa540931928c7dd100624f24fe7
TheRealRion <alyssambarker879@gmail.com>
2022-09-19 12:10:01 -0700

    athian's turn

---
+++ intro-lyssa-athian/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+    return ( 
+    <>
+        <h6> This page was created by Lyssa and Athian </h6> 
+    </> 
+    ) 
+}
+
+export default Footer
---
+++ intro-lyssa-athian/src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Profile = () => {
+    return ( 
+    <>
+        <h2> Authors</h2> 
+        <h4>Lyssa</h4>
+        <p> I am a mother of two kids, three kitties, and one husband.</p>
+        <h4>Athian</h4>
+        <p> I am a bookworm and enjoy drinking mint tea.</p>
+    </> 
+    ) 
+}
+
+export default Profile
