c9a565a815cb19b1dbb93a221ce8aa4412b0872b
Joseph <jhan.technology@gmail.com>
2022-09-19 16:41:44 -0700

    Half way with State-React challenge -heath & joseph

---
+++ state-heath-joseph/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import SquareBox from "./components/SquareBox"
+import React, { useState } from "react"
+
+
+const App = () => {
+  return (
+    <>
+      <h1>Square Box App</h1>
+      <SquareBox/>
+    </>
+  )
+}
+
+export default App
