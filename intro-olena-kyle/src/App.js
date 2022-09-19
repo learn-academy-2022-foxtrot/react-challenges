37cb8726ebbdcd9df0da5efdc772522757667f3c
let <kyle.tran.le@gmail.com>
2022-09-19 12:09:16 -0700

    challenge done

---
+++ intro-olena-kyle/src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Profile/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ intro-olena-kyle/src/components/Profile.js
@@ -0,0 +1,6 @@
+const Profile = () => {
+    return(
+        <h2> Kyle and Olena - Junior Developers </h2>
+    )
+}
+export default Profile
