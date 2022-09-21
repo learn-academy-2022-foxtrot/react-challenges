13d8982642049b7d1c5abb743aa9d39d77e71385
TheRealRion <alyssambarker879@gmail.com>
2022-09-21 15:04:29 -0700

    updated log

--- props-tawn-lyssa/src/App.js
+++ props-tawn-lyssa/src/App.js
@@ -1,26 +1,43 @@
 import './App.css'
 import Dice from './components/Dice'
-
+import Rolls from './components/Rolls'
 import React, {useState} from 'react'
 
 const App = () => {
   
-  const [dices, setDices] = useState(["One", "Two", "Three", "Four", "Five", "Six"])
+  const [dices, setDices] = useState([1,2,3,4,5,6])
 
+  //Creates rolls log 
+  const [rollsLog, setRollsLog] = useState([])
+
+  
+  
+  //Creates random number 
   const [randomIndex, setRandomIndex] = useState(0)
-
   const handleClick = () => {
     let randomNumber = Math.floor(Math.random() * dices.length)
     setRandomIndex(randomNumber)
+    setRollsLog(rollsLog.push(randomNumber))
+    console.log(typeof {rollsLog}, typeof {dices})
+    
 }
   
   
   return (
     <>
+
       <h1 className='title'>Dice Roll</h1>
-      <button onClick={handleClick}>Roll Dice</button>
-      <Dice dice={dices} index={randomIndex}/>
-    
+      <div className="diceBox"> 
+        <button onClick={handleClick}>Roll Dice</button>
+        <Dice dice={dices} index={randomIndex}/>
+      </div>
+      <div className="rollsLog">
+        
+        <Rolls log={rollsLog} />
+        <p>{rollsLog}</p>
+        
+        
+      </div>
     </>
 
 
---
+++ props-tawn-lyssa/src/components/Rolls.js
@@ -0,0 +1,11 @@
+const Rolls = (props) => {
+    return(
+        <>
+            <h2>This is the Rolls Log</h2>
+            <h3>{props.log}</h3>
+        
+        </>
+    )
+}
+
+export default Rolls
