8f6878332ec50c6b45a9160b09339b3ba6c9450e
guanhchan <guanh.chan@gmail.com>
2022-09-21 11:28:56 -0700

    update test

--- state-guan-ari/src/components/BoxButton.js
+++ state-guan-ari/src/components/BoxButton.js
@@ -1,11 +1,15 @@
-import { useCallback, useState } from "react"
+// import { useCallback, useState } from "react"
 import Colorbox from "./Colorbox"
 
 const BoxButton = () =>{
-    const [box,SetBox] = useState(0)
+    // const [box,SetBox] = useState(0)
     const boxMaker = () =>{
-
-        return (<Colorbox/>)
+console.log('test')
+        return (
+            <>
+        <h1>Hello</h1>
+ </>
+        )
 
     }
     return(
